807ab5f46024c41ac2f65cb5026ec5ca33152ae0
AndreaBATTI <andrea.battistini@capellinisauro.edu.it>
2025-05-12 06:48:24 +0000

    Aggiunta stampa di array statico

--- hello.java
+++ hello.java
@@ -7,6 +7,11 @@ public class hello{
         System.out.println("Hello Andrea!");
         System.out.println("Sono nato il 22 Agosto 2007");
         System.out.println("Non tifo alcuna squadra");
+        char []nome = {'A','n','d','r','e','a'};
+        for(int i=0; i<nome.length; i++){
+            System.out.print(nome[i]+" ");
+        }
+        System.out.println("");
     }
 }
 
