4a760e115032bc40d6bf96a6dac2a374fb414734
AndreaBATTI <andrea.battistini@capellinisauro.edu.it>
2025-05-12 06:57:32 +0000

    Aggiunta stampa verticale di un vettore

--- hello.java
+++ hello.java
@@ -1,4 +1,5 @@
 import java.io.*;
+import java.util.Vector;
 public class hello{
 
     public static void main(String[] args) {
@@ -7,6 +8,17 @@ public class hello{
         System.out.println("Hello Andrea!");
         System.out.println("Sono nato il 22 Agosto 2007");
         System.out.println("Non tifo alcuna squadra");
+        Vector<Character> v = new Vector<Character>();
+        v.add('a');
+        v.add('n');
+        v.add('d');
+        v.add('r');
+        v.add('e');
+        v.add('a');
+        for(int i = 0; i < v.size(); i++){
+            System.out.println(v.elementAt(i));
+        }
+        System.out.println("");
     }
 }
 
